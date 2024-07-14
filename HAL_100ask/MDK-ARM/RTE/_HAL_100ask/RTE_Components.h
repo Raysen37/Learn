8cea86c649a652ca8710dfc549b2fa4e3f61e5a7
Raysen <raysen37@gmail.com>
2024-07-14 17:06:06 +0800

    你的提交信息

---
+++ HAL_100ask/MDK-ARM/RTE/_HAL_100ask/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HAL_100ask' 
+ * Target:  'HAL_100ask' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
